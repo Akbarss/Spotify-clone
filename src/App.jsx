5919e17f8a07c638a670b434e5a04c0fa9a95af4
akbar gulyamov <kbrgulyamov@gmail.com>
2023-01-08 23:19:19 +0500

    feat: lib mui add done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import LazyLoad from 'react-lazy-load';
+import Button from '@mui/material/Button';
+import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <LazyLoad  height={762} width={400} threshold={0.95} onContentVisible={() => {console.log('loaded!')}}>
-        <img src='http://apod.nasa.gov/apod/image/1502/HDR_MVMQ20Feb2015ouellet1024.jpg' />
-      </LazyLoad>
-    </div>
+    <>
+          <Button variant="contained">Contained</Button>
+    </>
   )
 }
 
