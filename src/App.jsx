dc5446dcc66ccce041a5d209c8744323a602a8b4
akbar gulyamov <kbrgulyamov@gmail.com>
2023-01-08 22:54:00 +0500

    feat: add lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
+import LazyLoad from 'react-lazy-load';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-
+      <LazyLoad  height={762} width={400} threshold={0.95} onContentVisible={() => {console.log('loaded!')}}>
+        <img src='http://apod.nasa.gov/apod/image/1502/HDR_MVMQ20Feb2015ouellet1024.jpg' />
+      </LazyLoad>
     </div>
   )
 }
