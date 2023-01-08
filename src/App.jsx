ee30cd14a53a70d7c40845a87e3af38864961c7c
akbar gulyamov <kbrgulyamov@gmail.com>
2023-01-08 23:21:09 +0500

    feat: lib mui add done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 import Button from '@mui/material/Button';
 import './App.css'
+import React, { lazy, Suspense } from 'react';
+
+const renderLoader = () => <p>Loading</p>;
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {hasError: false};
+    }
+
+    static getDerivedStateFromError(error) {
+        return {hasError: true};
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <p>Loading failed! Please reload.</p>;
+        }
+
+        return this.props.children;
+    }
+}
 
 
 function App() {
