acd2fd2c4062fbad26cd1e86541ecdbc89cc4560
akbar gulyamov <kbrgulyamov@gmail.com>
2023-01-22 23:14:36 +0500

    feat: add SideBar

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -3,6 +3,7 @@ import React, { Suspense } from "react";
 import { useResource } from "../api/resource";
 import Posts from "../components/Cards/Posts/Posts";
 import Layout from "../components/Layout";
+import { CircularProgress, Box } from "@mui/material";
 
 let resource = useResource();
 
@@ -10,7 +11,19 @@ const HomePage = () => {
   return (
     <>
       <Layout>
-        <Suspense fallback={<p style={{ fontSize: "60px", textAlign: "center" }}>Loading</p>}>
+        <Suspense
+          fallback={
+            <Box
+              display={"flex"}
+              alignItems="center"
+              justifyContent={"center"}
+              width="100%"
+              height={"100vh"}
+            >
+              <CircularProgress size={"50px"} />
+            </Box>
+          }
+        >
           <Stack direction={"row"} flexWrap={"wrap"} gap={3} mt={15} justifyContent={"center"}>
             <Posts resource={resource} />
           </Stack>
